87190ba45fba3fc994194453c6467d1acd6097ac
sjwayrhz <sjwayrhz@gmail.com>
2025-12-25 11:06:48 +0800

    update docker-run-example.sh

--- ubuntu-in-docker/docker-run-example.sh
+++ ubuntu-in-docker/docker-run-example.sh
@@ -4,10 +4,10 @@
 # 本脚本展示如何使用环境变量运行 Ubuntu Cloudflare Tunnel 容器
 
 # 方法 1: 直接在命令行中指定环境变量
+
 docker run -d \
   --name ubuntu-tunnel \
   -e CLOUDFLARE_TUNNEL_TOKEN="your_cloudflare_tunnel_token_here" \
-  -p 22:22 \
   sjwayrhz/ubuntu:tunnel-v0.2
 
 # 方法 2: 使用环境变量文件
@@ -18,7 +18,6 @@ docker run -d \
 # docker run -d \
 #   --name ubuntu-tunnel \
 #   --env-file .env \
-#   -p 22:22 \
 #   sjwayrhz/ubuntu:tunnel-v0.2
 
 # 注意: 请将 "your_cloudflare_tunnel_token_here" 替换为你的实际 Cloudflare Tunnel token
