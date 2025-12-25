fbecc8e05bde0d34021de0c06a8aa55f7760d4c9
sjwayrhz <sjwayrhz@gmail.com>
2025-12-25 11:10:48 +0800

    update docker-run-example.sh

--- ubuntu-in-docker/docker-run-example.sh
+++ ubuntu-in-docker/docker-run-example.sh
@@ -21,3 +21,19 @@ docker run -d \
 #   sjwayrhz/ubuntu:tunnel-v0.2
 
 # 注意: 请将 "your_cloudflare_tunnel_token_here" 替换为你的实际 Cloudflare Tunnel token
+
+
+# 安装 Cloudflare CLI 工具
+# curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && sudo dpkg -i cloudflared.deb
+# 
+# 设置配置文件
+# vim .ssh/config
+# 
+# Host my_tunnel
+#     HostName ssh.hsafj.dpdns.org
+#     User root
+#         IdentityFile ~/.ssh/id_ed25519
+#     ProxyCommand /usr/bin/cloudflared access ssh --hostname %h
+# 
+# 运行 SSH 连接
+# ssh my_tunnel
